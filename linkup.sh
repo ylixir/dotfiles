aec40a2c8317523dce13876c1b90ab770ef4082c
Jon Allen <ylixir@gmail.com>
2015-01-04 13:48:20 -0600

    merge more

--- linkup.sh
+++ linkup.sh
@@ -8,9 +8,6 @@ ln -sf `pwd`/gvimrc ~/.gvimrc
 ln -sf `pwd`/xinitrc ~/.xinitrc
 ln -sf `pwd`/herbstluftwm ~/.config/
 ln -sf `pwd`/fonts ~/.fonts
-<<<<<<< HEAD
 ln -sf `pwd`/bin ~/bin
-=======
 ln -sf `pwd`/terminfo ~/.terminfo
->>>>>>> 0bb49fe57fbf2722738884872ffcf6f10c77ff07
 fc-cache -fv
