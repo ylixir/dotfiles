a6c63027d7c688f99a70c3554eeacaa03ab45c0f
Jon Allen <ylixir@gmail.com>
2015-01-04 13:54:32 -0600

    bin path

--- linkup.sh
+++ linkup.sh
@@ -6,8 +6,9 @@ ln -sf `pwd`/gitconfig ~/.gitconfig
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gvimrc ~/.gvimrc
 ln -sf `pwd`/xinitrc ~/.xinitrc
+ln -sf `pwd`/profile ~/.profile
 ln -sf `pwd`/herbstluftwm ~/.config/
 ln -sf `pwd`/fonts ~/.fonts
 ln -sf `pwd`/bin ~/bin
 ln -sf `pwd`/terminfo ~/.terminfo
-fc-cache -fv
+fc-cache -f
