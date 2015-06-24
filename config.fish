c21cd30013ba20ec63a7a36471689e467bf14d2a
Jon Allen <ylixir@gmail.com>
2015-06-24 10:32:58 -0500

    gpg and chrome

--- config.fish
+++ config.fish
@@ -5,3 +5,7 @@ set PATH ~/bin /usr/local/bin /bin /usr/bin /usr/local/sbin /sbin /usr/sbin /opt
 #. /home/ylixir/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
 
 set LANG fr_FR.UTF-8
+
+set -x CHROME_FLAGS "--ppapi-flash-path=/usr/lib/chromium-pepper-flash/libpepflashplayer.so --ppapi-flash-version=17.0.0.134"
+
+set -x GPGKEY 29A032C3
