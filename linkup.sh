5d17ea8d5ed1a5397fe59259c7ccbe84d50f9f6e
Jon Allen <ylixir@gmail.com>
2016-09-17 09:41:32 -0500

    added mononoki, made linkup a little less aggressive

--- linkup.sh
+++ linkup.sh
@@ -4,14 +4,14 @@ ln -sf `pwd`/vim ~/.vim
 ln -sf `pwd`/gitconfig ~/.gitconfig
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gvimrc ~/.gvimrc
-ln -sf `pwd`/xinitrc ~/.xinitrc
+#ln -sf `pwd`/xinitrc ~/.xinitrc
 ln -sf `pwd`/profile ~/.profile
 ln -sf `pwd`/herbstluftwm ~/.config/
 ln -sf `pwd`/fonts ~/.fonts
 ln -sf `pwd`/bin ~/bin
 ln -sf `pwd`/terminfo ~/.terminfo
 ln -sf `pwd`/tmux.conf ~/.tmux.conf
-ln -sf `pwd`/llpp.conf ~/.config/llpp.conf
-ln -sf `pwd`/config.fish ~/.config/fish/config.fish
+#ln -sf `pwd`/llpp.conf ~/.config/llpp.conf
+#ln -sf `pwd`/config.fish ~/.config/fish/config.fish
 ln -sf `pwd`/fish\ functions/ssh_agent_start.fish ~/.config/fish/functions/
 fc-cache -f
