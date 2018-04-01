bbdeaccfdae0a76d0cdf8384315c82140b7ba74d
Jon ylixir Allen <ylixir@gmail.com>
2018-03-31 22:48:53 -0500

    fish is login/interactive, not default

--- setup-cli.sh
+++ setup-cli.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 nix-env -f cli.nix -i --remove-all
 
-#this is gross
-grep -F `which fish` /etc/shells
-if [ $? -ne 0 ]; then
-  which fish | sudo tee -a /etc/shells
+
+#setup fish. note that this is very bash centric.
+#mostly bash is the default, and our game is just
+#to be able to use fish, not make it the "default"
+
+# make sure that we fire bashrc, even on a login
+if [ -e $HOME/.bash_profile ]; then
+  profile="$HOME/.bash_profile"
+elif [ -e $HOME/.bash_login ]; then
+  profile="$HOME/.bash_login"
+else
+  profile="$HOME/.profile"
 fi
-chsh -s `which fish`
+grep -F bashrc $profile
+if [ $? -ne 0 ]; then
+  printf ". %s\n" $HOME/.bashrc >> $profile
+fi
+
+# now make sure bash starts fish when running from login mode
+grep -F fish $HOME/.bashrc
+if [ $? -ne 0 ]; then
+  printf "exec %s\n" `which fish` >> $HOME/.bashrc
+fi
