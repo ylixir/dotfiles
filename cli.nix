24412f44b061b7337fb94bf26f62c9574c0ce5fb
Jon Allen <ylixir@gmail.com>
2018-10-29 20:37:46 +0100

    experimenting with tmux

--- cli.nix
+++ cli.nix
@@ -1,16 +1,36 @@
 let
   # https://unix.stackexchange.com/questions/183440/how-do-i-create-a-local-repository-for-nixos-nixpkgs
   pkgs = import <nixpkgs> {};
+  config.programs.tmux = {
+      enable = true;
+      clock24 = true;
+      keyMode = "vi";
+      terminal = "screen-256color";
+      shortcut = "a";
+      extraTmuxConf = ''
+        run-shell ${pkgs.tmuxPlugins.prefix-highlight}/share/tmux-plugins/prefix-highlight/prefix_highlight.tmux
+        run-shell ${pkgs.tmuxPlugins.sidebar}/share/tmux-plugins/sidebar/sidebar.tmux
+        run-shell ${pkgs.tmuxPlugins.urlview}/share/tmux-plugins/urlview/urlview.tmux
+        run-shell ${pkgs.tmuxPlugins.yank}/share/tmux-plugins/yank/yank.tmux
+        run-shell ${pkgs.tmuxPlugins.pain-control}/share/tmux-plugins/pain-control/pain_control.tmux
+        run-shell ${pkgs.tmuxPlugins.logging}/share/tmux-plugins/logging/logging.tmux
+        run-shell ${pkgs.tmuxPlugins.open}/share/tmux-plugins/open/open.tmux
+        run-shell ${pkgs.tmuxPlugins.copycat}/share/tmux-plugins/copycat/copycat.tmux
+        ${builtins.readFile ./configs/tmux.conf}
+      '';
+    };
   homies = with pkgs; [
     fish
     nix
     tmux
+    reattach-to-user-namespace
     ];
   tmux = import ./tmux (with pkgs;
     { inherit
         makeWrapper
         symlinkJoin
         writeText
+	tmuxPlugins
         ;
       tmux = pkgs.tmux;
     });
--- tmux/default.nix
+++ tmux/default.nix
@@ -1,11 +1,29 @@
 # Tmux with ./tmux.conf baked in
-{ tmux, writeText, symlinkJoin, makeWrapper }:
+{ tmux, tmuxPlugins, writeText, symlinkJoin, makeWrapper }:
+let
+  customConf = builtins.toFile "custom.conf" ''
+    ${builtins.readFile ./base.conf}
+    run-shell ${tmuxPlugins.yank}/share/tmux-plugins/yank/yank.tmux
+  '';
+in
 symlinkJoin {
   name = "tmux";
   buildInputs = [makeWrapper];
   paths = [ tmux ];
   postBuild = ''
+    mkdir $out/etc
+
+    cat << EOF > $out/etc/tmux.conf
+
+    ${builtins.readFile ./base.conf}
+    set-option -g default-command "reattach-to-user-namespace -l bash"
+    run-shell ${tmuxPlugins.yank}/share/tmux-plugins/yank/yank.tmux
+    bind-key -t vi-copy MouseDragEnd1Pane copy-pipe "reattach-to-user-namespace pbcopy"
+    bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel "reattach-to-user-namespace pbcopy"
+
+    EOF
+
     wrapProgram "$out/bin/tmux" \
-    --add-flags "-f ${./tmux.conf}"
+      --add-flags "-f $out/etc/tmux.conf"
   '';
 }
