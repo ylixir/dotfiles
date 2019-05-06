d1309dadbc32a045a512e2ef6cc570fccb3640ba
Jon Allen <jon@ylixir.io>
2019-05-06 23:15:03 +0200

    better thefuck integration

--- fish.nix
+++ fish.nix
@@ -41,8 +41,6 @@ pkgs: {
       end
     end
 
-    thefuck --alias | source
-
     if [ -n "$SSH_AGENT_PID" ] 
       ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
       if [ $status -eq 0 ]
@@ -68,6 +66,16 @@ pkgs: {
 
     function fish_user_key_bindings
       fzf_key_bindings
+    end
+
+    function fuck -d "Correct your previous console command"
+      set -l fucked_up_command $history[1]
+      env TF_SHELL=fish TF_ALIAS=fuck PYTHONIOENCODING=utf-8 thefuck $fucked_up_command | read -l unfucked_command
+      if [ "$unfucked_command" != "" ]
+        eval $unfucked_command
+        builtin history delete --exact --case-sensitive -- $fucked_up_command
+        builtin history merge ^ /dev/null
+      end
     end
     '';
 
