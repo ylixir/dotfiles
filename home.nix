c2ca0c165d292d19e64a8e129f50a75a2b86e376
Jon Allen <jon@ylixir.io>
2020-04-13 22:38:41 -0400

    bspwm

--- home.nix
+++ home.nix
@@ -5,27 +5,29 @@ in {
   programs.home-manager.enable = true;
   home.packages = with pkgs; [
     (import ./lorri {})
-    ag
     adoptopenjdk-bin
+    ag
     bash #TODO open pr to fix this with home-manager
     cargo
-    thefuck
     gnumake
-    nodePackages.node2nix
     node-pkgs."@elm-tooling/elm-language-server"
     node-pkgs.intelephense # php language server
     node-pkgs.javascript-typescript-langserver
     node-pkgs.prettier
     node-pkgs.typescript-tslint-plugin
+    nodePackages.node2nix
     nodejs_latest
     ps
     solargraph # ruby language server
+    thefuck
   ] ++ (if pkgs.system == "x86_64-darwin" then [] else [ (import neovim/gtk.nix pkgs) ]) ;
 
   home.file = {
     ".config/fish/functions/fzf_key_bindings.fish".source = builtins.fetchurl "https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.fish";
     ".config/nvim/init.vim".source = neovim/init.vim;
     ".config/nvim/coc-settings.json".source = neovim/coc-settings.json;
+    ".xinitrc".source = bspwm/xinitrc;
+    ".config/sxhkd/sxhkdrc".source = sxhkd/sxhkdrc;
   };
   programs.bash = (import ./bash.nix pkgs);
   programs.bat.enable = true;
